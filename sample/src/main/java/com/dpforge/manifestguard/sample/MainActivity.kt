661b5a3b48d9b040383f33891e4ff496dfe19ac1
Aleksandr Suslov <subprogram@gmail.com>
2025-11-06 17:44:30 +0100

    Update dependencies in sample module, bump target sdk value: 32 -> 36

--- sample/src/main/java/com/dpforge/manifestguard/sample/MainActivity.kt
+++ sample/src/main/java/com/dpforge/manifestguard/sample/MainActivity.kt
@@ -2,10 +2,15 @@ package com.dpforge.manifestguard.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Enable edge-to-edge display for Android 15+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContentView(R.layout.activity_main)
     }
 }
